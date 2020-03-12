0b6ca41eb1b57224ed722d7575baf63edea56ab9
lisseteventura <lisseteventura@gmail.com>
2020-03-12 12:49:24 -0400

    added on change event to have both inputs change upon input

--- src/App.js
+++ src/App.js
@@ -12,6 +12,19 @@ function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
   const [fromCurrency, setFromCurrency] = useState();
   const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+
   //first param = function
   //second param = array
   //whenever the items in the array change, we want to rerun the effect
@@ -24,8 +37,20 @@ function App() {
         setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
         setFromCurrency(data.base);
         setToCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency]);
       });
   }, []);
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
+
   return (
     <>
       <h1>Convert</h1>
@@ -33,12 +58,16 @@ function App() {
         currencyOptions={currencyOptions}
         selectedCurrency={fromCurrency}
         onChangeCurrency={e => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
       />
       <div className="equals"> = </div>
       <CurrencyRow
         currencyOptions={currencyOptions}
         selectedCurrency={toCurrency}
         onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
       />
     </>
   );
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 const CurrencyRow = props => {
-  const { currencyOptions, selectedCurrency, onChangeCurrency } = props;
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount
+  } = props;
   return (
     <div>
-      <input type="number" className="input" />
+      <input
+        type="number"
+        className="input"
+        value={amount}
+        onChange={onChangeAmount}
+      />
       <select value={selectedCurrency} onChange={onChangeCurrency}>
         {/* loop through currencyOptions */}
         {currencyOptions.map(option => (
